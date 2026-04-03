a59428487478b99e5d325097ff87c34d162a2ad0
Samuel K. Kihoon <SamuelTheGalaxys@gmail.com>
2026-04-03 02:19:24 -0400

    ...

--- team.js
+++ team.js
@@ -6,7 +6,7 @@
       {
         name: 'Samuel Kihoon',
         role: 'CEO',
-        image: 'https://pbs.twimg.com/profile_images/1796106702640893952/vvZb6LgR_400x400.jpg',
+        image: 'resources/samuelkihoon.jpeg',
         linkedin: 'https://linkedin.com/in/Kihoonx',
         description: 'As Birzont\'s CEO and CTO, he is building the future of AI technology. He studied Business Administration and Big Data Applications at Kyung Hee University, worked as a Notion seasonal intern and CTO at careermizing.com, and now leads Birzont.'
       },
@@ -18,7 +18,7 @@
         description: 'Detailed information about this team member will be shown here.'
       },
       {
-        name: 'Eva Lee (Yena)',
+        name: 'Eva Lee',
         role: 'CTO',
         image: 'resources/evayenalee.png',
         linkedin: 'https://www.linkedin.com/in/%EC%98%88%EB%82%98-%EC%9D%B4-333271315/',
@@ -40,17 +40,17 @@
       },
       {
         name: 'Jinwoo Jung',
-        role: 'Intern',
-        image: 'resources/jinwoo.png',
+        role: 'Marketing',
+        image: 'resources/jinwoo.jpeg',
         linkedin: '#',
         description: 'Detailed information about this team member will be shown here.'
       }
     ],
     ko: [
       {
-        name: '새뮤얼 기훈 김',
-        role: '대표이사 (CEO)',
-        image: 'https://pbs.twimg.com/profile_images/1796106702640893952/vvZb6LgR_400x400.jpg',
+        name: '김기훈',
+        role: '대표이사',
+        image: 'resources/samuelkihoon.jpeg',
         linkedin: 'https://linkedin.com/in/Kihoonx',
         description: '버존트의 CEO이자 CTO로서 AI 기술의 미래를 만들어가고 있습니다. 학력은 경희대학교 경영대학 및 빅데이터응용학과로, Notion 시즌인턴, 커리어마이징닷컴 CTO로 있었으며, 현재 버존트를 위해 일하고 있습니다.'
       },
@@ -62,8 +62,8 @@
         description: '팀 멤버에 대한 상세 정보가 여기에 표시됩니다.'
       },
       {
-        name: '에바 예나 리 (CTO)',
-        role: '최고기술책임자',
+        name: '이예나',
+        role: '최고기술책임자 (CTO)',
         image: 'resources/evayenalee.png',
         linkedin: 'https://www.linkedin.com/in/%EC%98%88%EB%82%98-%EC%9D%B4-333271315/',
         description: '팀 멤버에 대한 상세 정보가 여기에 표시됩니다.'
@@ -84,8 +84,8 @@
       },
       {
         name: '정진우',
-        role: '마케팅 인턴',
-        image: 'resources/jinwoo.png',
+        role: '마케팅',
+        image: 'resources/jinwoo.jpeg',
         linkedin: '#',
         description: '팀 멤버에 대한 상세 정보가 여기에 표시됩니다.'
       }
